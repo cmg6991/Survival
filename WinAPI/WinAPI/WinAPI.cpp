fd53e55534d40c906ad520092e4e0220671292e5
cmg6991 <110959882+cmg6991@users.noreply.github.com>
2026-08-21 03:32:11 +0900

    커밋

--- WinAPI/WinAPI/WinAPI.cpp
+++ WinAPI/WinAPI/WinAPI.cpp
@@ -14,7 +14,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
-    //_CrtSetBreakAlloc(8405);
+    //_CrtSetBreakAlloc(8218);
     //// TODO: 여기에 코드를 입력합니다.
 //#ifdef _DEBUG
 //    _CrtSetDbgFlag(
